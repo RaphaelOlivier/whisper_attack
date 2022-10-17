b96944fd8ed3a1e16659a505516068a6fa670e5e
Raphael Olivier <raphael.franck.olivier@gmail.com>
2022-10-17 17:58:30 -0400

    scripts edits

--- cw.sh
+++ cw.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 source activate sp3
-NBITER=1000
-SEED=1004
+NBITER=2000
+SEED=1001
 LOAD=False
 NAME=cw
-EPS=0.015
-CST=10
-
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=tiny --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=tiny.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=base.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=small --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=small.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=medium --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=medium.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
-python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=test-clean-20 --model_label=large --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST
+EPS=0.1
+CST=100
+MAXDECR=3
+DATA=test-clean-20
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=tiny.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=small.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium.en --tokenizer_name=gpt2 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
+#python evaluate.py cw.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=large --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME --eps=$EPS --const=$CST --max_decr=$MAXDECR
--- gpu.sh
+++ gpu.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=cw_res.txt
 #
 #SBATCH --ntasks=1
-#SBATCH --partition=gpu2
-#SBATCH --nodelist=compute-2-13
+#SBATCH --partition=gpu
+#SBATCH --nodelist=compute-2-5
 #SBATCH --gres=gpu:3 
 #SBATCH --mem=100g 
 #SBATCH --time=72:00:00
