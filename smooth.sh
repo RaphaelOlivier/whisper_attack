6ee2c07b6be278172e78d7170ed641621d769a0e
Raphael Olivier <raphael.franck.olivier@gmail.com>
2022-10-26 12:30:23 -0400

    organize repo into subfolders

--- smooth.sh
+++ smooth.sh
@@ -7,12 +7,12 @@ NBITER=200
 
 SNR=35
 SEED=335
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
 
 SNR=40
 SEED=340
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python evaluate.py pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+python evaluate.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
