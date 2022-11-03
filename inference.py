265e6b92cff28130e2c6f8c5630f191453dd7f4c
Raphael Olivier <raphael.franck.olivier@gmail.com>
2022-11-02 22:35:06 -0400

    support for cpu and streaming datasets

--- inference.py
+++ inference.py
@@ -45,6 +45,8 @@ parser.add_argument('-m','--model', help='Whisper model to use', default="tiny")
 parser.add_argument('-c','--config', help='Dataset config to use', default="targeted")
 parser.add_argument('-s','--split', help='Dataset split to use for the given config', default=None)
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+STREAMING=False
 
 def main(args):
     language=None
@@ -72,34 +74,32 @@ def main(args):
             ), "Split %s invalid with config %s"%(args.split,args.config)
     evaluate_on_adv_examples(args.model,args.config,args.split,language=language)
 
-# load dummy dataset and read soundfiles
-
 def evaluate_on_adv_examples(model_name,config_name,split_name,language=None):
-
     hub_path = "openai/whisper-"+model_name
     processor = WhisperProcessor.from_pretrained(hub_path)
-    model = WhisperForConditionalGeneration.from_pretrained(hub_path).to("cuda")
+    model = WhisperForConditionalGeneration.from_pretrained(hub_path).to(DEVICE)
     if language is not None:
         warnings.warn("Whisper on Huggingface does not support language detection, and seems to default to english in presence of any language. We set the language token to %s, which affects results if using the language detection attack."%language)
         model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(language=language,task = "transcribe")
-
-    dataset = load_dataset("whisper_adversarial_examples",config_name ,split=split_name)
+  
+    dataset = load_dataset("whisper_adversarial_examples",config_name ,split=split_name, streaming=STREAMING)
 
     def map_to_pred(batch):
         input_features = processor(batch["audio"][0]["array"], return_tensors="pt").input_features
-        predicted_ids = model.generate(input_features.to("cuda"))
+        predicted_ids = model.generate(input_features.to(DEVICE))
         transcription = processor.batch_decode(predicted_ids, normalize = True)
         batch['text'][0] = processor.tokenizer._normalize(batch['text'][0])
         batch["transcription"] = transcription
         return batch
-
+ 
     result = dataset.map(map_to_pred, batched=True, batch_size=1)
-
+    if STREAMING:
+        result = list(result)
+        result = {key: [r[key] for r in result] for key in result[0]}
     wer = load("wer")
     for t in zip(result["text"],result["transcription"]):
         print(t)
-    print(wer.compute(predictions=result["text"], references=result["transcription"]))
-
+    print(wer.compute(predictions=result["transcription"], references=result["text"]))
 
 if __name__=="__main__":
     args = parser.parse_args()
