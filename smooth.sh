722a14051bdaf4652f225b9632c73030926159d6
Raphael Olivier <raphael.franck.olivier@gmail.com>
2023-05-15 23:17:47 -0400

    smoothing

--- smooth.sh
+++ smooth.sh
@@ -4,14 +4,17 @@ DATA=test-clean-100
 NAME=pgd
 NBITER=200
 
-SNR=35
-SEED=335
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+for SNR in 10 15 20 25 30 35 40 45 50
+do
+    python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=100 --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+    python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=200 --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+done
+    
+for SNR in 10 15 20 25 30 35 40 45 50
+do
+    python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium --nb_iter=100 --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+    python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=medium.smooth.02 --nb_iter=200 --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
+done
+    
+    
 
-SNR=40
-SEED=340
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.02 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
-python run_attack.py attack_configs/pgd.yaml --root=$RSROOT --data_csv_name=$DATA --model_label=base.smooth.03 --nb_iter=$NBITER --load_audio=$LOAD --seed=$SEED --attack_name=$NAME  --snr=$SNR
